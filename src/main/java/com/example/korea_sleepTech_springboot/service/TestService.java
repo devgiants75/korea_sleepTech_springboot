e758484013d7a8e7b9ec43b03ef4fefb8cdf5c0e
devgiants75 <devgiants75@naver.com>
2025-04-28 16:23:44 +0900

    Spring Boot test

---
+++ src/main/java/com/example/korea_sleepTech_springboot/service/TestService.java
@@ -0,0 +1,23 @@
+package com.example.korea_sleepTech_springboot.service;
+
+import com.example.korea_sleepTech_springboot.entity.TestEntity;
+import com.example.korea_sleepTech_springboot.repository.TestRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+    // TestService가 실행(인스턴스화)되면 자동으로 testRepository에 의존성이 주입되기 때문에
+    // : final로 반드시 객체 초기화
+    private final TestRepository testRepository;
+
+    // 생성자 주입 방식
+    public TestService(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
+    public List<TestEntity> getAllTests() {
+        return testRepository.findAll(); // 전체 test 목록을 가져오기
+    }
+}
