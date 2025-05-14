e9dc6729d77be9f19a01a5d5bb2bdd361795798f
devgiants75 <devgiants75@naver.com>
2025-05-14 12:38:07 +0900

    20250514 Spring Security authority

--- src/main/java/com/example/korea_sleepTech_springboot/entity/UserRole.java
+++ src/main/java/com/example/korea_sleepTech_springboot/entity/UserRole.java
@@ -5,15 +5,18 @@ package com.example.korea_sleepTech_springboot.entity;
 // >> Spring JPA에서 다대다(@ManyToMany) 관계는 기본적으로 중간 테이블을 자동 관리
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 // +) user_roles 테이블을 엔티티로 만들어야 하는 경우
 //      : 중간 테이블에 추가 정보가 존재하는 경우
 //      >> 역할 부여 날짜(assigned_at), 역할의 상태(is_active), 부여자 ID(assigned_by) 등등
-@Entity
-@Table(name = "user_roles")
+//@Entity
+//@Table(name = "user_roles")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRole {
     @EmbeddedId
     private UserRoleId id; // 복합 키
@@ -29,7 +32,7 @@ public class UserRole {
     private Role role;
 
 //    private LocalDateTime assignAt;
-    
+
     @Builder
     public UserRole(User user, Role role) {
         this.user = user;
