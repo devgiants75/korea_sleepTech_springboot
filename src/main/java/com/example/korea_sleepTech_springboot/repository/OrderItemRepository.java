802336bee7040b860ad54dbafcbf4eb7590a821d
devgiants75 <devgiants75@naver.com>
2025-05-27 10:50:47 +0900

    20250527 SpringBoot(Transaction, Trigger, View, Index)

---
+++ src/main/java/com/example/korea_sleepTech_springboot/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.korea_sleepTech_springboot.repository;
+
+import com.example.korea_sleepTech_springboot.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
---
+++ src/main/java/com/example/korea_sleepTech_springboot/repository/OrderLog.java
@@ -0,0 +1,6 @@
+package com.example.korea_sleepTech_springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderLog extends JpaRepository<OrderLog, Long> {
+}
